105e1f1b748f2dfe22194e471ac472fc082c0879
Bilal <el.famoso.remedo@gmail.com>
2022-04-21 17:49:25 +0200

    ébauche serveur tcp/udp

--- server.c
+++ server.c
@@ -10,94 +10,185 @@
 #include <netdb.h>
 #include <signal.h>
 
+#define MAX_ID 8
 
-int main(int argc, char* argv[]) {
-    // Initialize the server 
-    server_init();
-    // Start the server
-    server_start();
-    // Cleanup the server
-    server_cleanup();
-    return 0;
-}
+struct user_info
+{
+    int sock_tcp;
+    int sock_udp;
+    char* tag;
+    uint16_t max;
+    uint32_t sin_addr;
+};
 
-// mutithread tcp server
-int connect(){
-    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
-    struct sockaddr_in my_addr;    // my address information
-    struct sockaddr_in their_addr; // connector's address information
-    socklen_t sin_size;
-    struct sigaction sa;
-    int yes=1;
-    char s[INET_ADDRSTRLEN];
-    int rv;
+void* comm(void* sock2);
 
-    // Create a socket
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("socket");
-        exit(1);
+int main(int argc, char* argv[])
+{
+    // Déclaration des variables
+    int port_tcp, port_udp, sock_tcp, sock_udp, sock2, sock3, size;
+    struct sockaddr_in sockaddress_tcp, sockaddress_udp, caller;
+    struct user_info info;
+    pthread_t th;
+
+    // Parsing de la ligne de commandes
+    if(3 != argc)
+    {
+        printf("UTILISATION\n./serveur port_tcp\n");
+        return 0;
     }
+    port_tcp = atoi(argv[1]);
+    port_udp = atoi(argv[2]);
+    printf("Ligne de commande parsée.\n");
 
-    // Set socket options
-    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-        perror("setsockopt");
-        exit(1);
+    // Création du socket TCP
+    sock_tcp = socket(PF_INET, SOCK_STREAM, 0);
+    if(sock_tcp == -1)
+    {
+        printf("Erreur lors de la création du socket.\n");
+        return 1;
     }
+    sockaddress_tcp.sin_family = AF_INET;
+    sockaddress_tcp.sin_port = htons(port_tcp);
+    sockaddress_tcp.sin_addr.s_addr = htonl(INADDR_ANY);
+    printf("Socket TCP créée.\n");
 
-    // Set up the address structure
-    my_addr.sin_family = AF_INET;         // host byte order
-    my_addr.sin_port = htons(4244);       // short, network byte order
-    my_addr.sin_addr.s_addr = INADDR_ANY; // automatically fill with my IP
-    memset(&(my_addr.sin_zero), '\0', 8); // zero the rest of the struct
-
-    // Bind the socket
-    if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr)) == -1) {
-        perror("bind");
-        exit(1);
+    // Création du socket UDP
+    sock_udp = socket(PF_INET, SOCK_STREAM, 0);
+    if(sock_udp == -1)
+    {
+        printf("Erreur lors de la création du socket.\n");
+        return 1;
     }
+    sockaddress_udp.sin_family = AF_INET;
+    sockaddress_udp.sin_port = htons(port_udp);
+    sockaddress_udp.sin_addr.s_addr = htonl(INADDR_ANY);
+    printf("Socket UDP créée.\n");
 
-    // Listen for connections
-    if (listen(sockfd, 8) == -1) {
-        perror("listen");
-        exit(1);
+
+    // Binding
+    if(bind(sock_tcp, (struct sockaddr *) &sockaddress_tcp, sizeof(struct sockaddr_in)) < 0) // TODO: ??? sizeof
+    {
+        perror("Erreur lors du binding du serveur avec la socket TCP.\n");
+        return 1;
     }
-
-    // Accept a connection
-    sin_size = sizeof(struct sockaddr_in);
-    if ((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size)) == -1) {
-        perror("accept");
-        exit(1);
+    if(bind(sock_udp, (struct sockaddr *) &sockaddress_udp, sizeof(struct sockaddr_in)) < 0) // TODO: ??? sizeof
+    {
+        perror("Erreur lors du binding du serveur avec la socket UDP.\n");
+        return 1;
     }
+    printf("Binding fait.\n");
 
-    // Get the IP address of the client
-   // inet_ntop(their_addr
-}
-
-
-
-int main() {
-    int sock=socket(PF_INET,SOCK_STREAM,0);
-    struct sockaddr_in sockaddress;
-    sockaddress.sin_family=AF_INET;
-    sockaddress.sin_port=htons(4244);
-    sockaddress.sin_addr.s_addr=htonl(INADDR_ANY);
-    int r=bind(sock,(struct sockaddr *)&sockaddress,sizeof(struct sockaddr_in));
-    if(r==0){
-        r=listen(sock,0);
-        if(r==0){
-            struct sockaddr_in caller;
-            socklen_t size=sizeof(caller);
-            while(1){
-                int *sock2=(int *)malloc(sizeof(int));
-                *sock2=accept(sock,(struct sockaddr *)&caller,&size);
-                if(sock2>=0){
-                    pthread_t th;
-                    pthread_create(&th,NULL,comm,sock2);
-                }
-            }
+    // Listen
+    if(listen(sock_tcp, 3) != 0 && listen(sock_udp, 3) != 0)
+    {
+        perror("Erreur lors de la création du serveur.\n");
+        return 1;
+    }
+    printf("En attente d'une connexion...\n");
+    
+    size = sizeof(struct sockaddr_in);
+    while(1)
+    {
+        sock2 = accept(sock_tcp, (struct sockaddr*) &caller, (socklen_t*) &size);
+        sock3 = accept(sock_udp, (struct sockaddr*) &caller, (socklen_t*) &size);
+        printf("Connexion acceptée.\n");
+        info.sock_tcp = sock2;
+        info.sock_udp = sock3;
+        info.sin_addr = caller.sin_addr.s_addr;
+        if(pthread_create(&th, NULL, comm, (void*) &info) < 0)
+        {
+            perror("Erreur lors de la création du thread.\n");
+            return 1;
         }
     }
+    if(sock2 < 0)
+    {
+        perror("Connexion échouée.\n");
+        return 1;
+    }
     return 0;
 }
 
+void* comm(void* info)
+{
+    // Déclaration des variables
+    struct user_info* current_user = (struct user_info*) info;
+    int sock_tcp = current_user -> sock_tcp;
+    int sock_udp = current_user -> sock_udp;
+    int read_size;
+    uint16_t number;
+    char client_message[2000], answer[17], tag[16];
+    struct sockaddr *saddr = current_user -> ai_addr;
+     
+    while(1)
+    {
+        // Lecture d'un message TCP
+        printf("Je cherche à lire un message en TCP.\n");
+        read_size = read(sock_tcp, tag, 99 * sizeof(char));
+        tag[read_size] = '\0';
 
+        // Vérification du message
+        if(read_size == 0)
+        {
+            printf("Client déconnecté.\n");
+            return 0;
+        }
+        else if(read_size == -1)
+        {
+            perror("La réception du message a échoué.\n");
+            return NULL;
+        }
+
+        // Témoin
+        printf("Message reçu en TCP : '%s'\n", tag);
+    
+        // Réponse en TCP
+        memcpy(answer, tag, strlen(tag));
+        memcpy(answer + strlen(tag) - 1, "feur", strlen("feur"));
+        answer[16] = '\0';
+        if(write(sock_tcp, answer, strlen(answer)) == -1)
+        {
+            perror("Erreur lors du hello au pseudo reçu.\n");
+            return NULL;
+        }
+
+        // Témoin
+        printf("Réponse envoyée en TCP : '%s'\n", answer);
+
+        // Lecture d'un message UDP
+        printf("Je cherche à lire un message en UDP.\n");
+        read_size = recv(sock_udp, tag, 99 * sizeof(char), 0);
+        tag[read_size] = '\0';
+
+        // Vérification du message
+        if(read_size == 0)
+        {
+            printf("Client déconnecté.\n");
+            return 0;
+        }
+        else if(read_size == -1)
+        {
+            perror("La réception du message a échoué.\n");
+            return NULL;
+        }
+
+        // Témoin
+        printf("Message reçu en UDP : '%s'\n", tag);
+    
+        // Réponse en UDP
+        memcpy(answer, tag, strlen(tag));
+        memcpy(answer + strlen(tag) - 1, "feur", strlen("feur"));
+        answer[16] = '\0';
+        if(sendto(sock_udp, answer, strlen(answer), 0, saddr, (socklen_t) sizeof(struct sockaddr_in)) == -1)
+        {
+            perror("Erreur lors du hello au pseudo reçu.\n");
+            return NULL;
+        }
+
+        // Témoin
+        printf("Réponse envoyée en UDP : '%s'\n", answer);
+
+    }
+    return 0;
+}
